b8cc8e1128c98356c2bd8604fe14ade38f34f6ae
AllanJSM <allan.magalhaes@mesainc.com.br>
2025-03-26 16:38:11 -0300

    Ajuste de query SQL

--- src/generateClosingSQL.js
+++ src/generateClosingSQL.js
@@ -27,8 +27,8 @@ function generateClosingSQL({
         if (cleanCNPJ.length === 14 && typeof value === 'number' && !isNaN(value)) {
             const isLast = index === rows.length - 1;
             const dateExpr = isLastYear
-                ? "(CURRENT_DATE - interval '13 month') + interval '14 hour'"
-                : "(CURRENT_DATE - interval '1 month') + interval '14 hour'";
+                ? "(date_trunc('month', CURRENT_DATE) - interval '12 month') - interval '1 day' + interval '14 hour'"
+                : "date_trunc('month', CURRENT_DATE) - interval '1 day' + interval '14 hour'";
             const sql = `SELECT ${dateExpr}, ${value}, t.id FROM ${table} t WHERE t.cnpj = '${cleanCNPJ}'${isLast ? '' : ' union all'}\n`;
             fs.appendFileSync(outputPath, sql);
         }
--- src/generateSQL.js
+++ src/generateSQL.js
@@ -27,7 +27,7 @@ function generateSellinSQL({
         if (cleanCNPJ.length === 14 && typeof value === 'number' && !isNaN(value)) {
             const isLast = index === rows.length - 1;
             const dateExpr = isLastYear
-                ? "(CURRENT_DATE - interval '12 month') + interval '14 hour'"
+                ? "(date_trunc('month', CURRENT_DATE) - interval '11 month') - interval '1 day' + interval '14 hour'"
                 : "CURRENT_DATE + interval '14 hour'";
             const sql = `SELECT ${dateExpr}, ${value}, t.id FROM ${table} t WHERE t.cnpj = '${cleanCNPJ}'${isLast ? '' : ' union all'}\n`;
             fs.appendFileSync(outputPath, sql);
